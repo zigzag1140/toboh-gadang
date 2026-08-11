829825e3b0e76a406f877fb00f596e629f121b3b
r.afdal <rahmadatul68@gmail.com>
2026-08-11 15:39:25 +0700

    Memperbaiki parameter URL pada PrismaClient

--- src/app/api/berita/route.ts
+++ src/app/api/berita/route.ts
@@ -4,7 +4,13 @@ import { PrismaClient } from "@prisma/client";
 // getSemuaBerita dan simpanBerita dihapus karena diganti Prisma
 import { getTagColorForCategory } from "@/lib/beritaStorage";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 const ADMIN_SESSION_TOKEN = "toboh_gadang_admin_auth_valid_session_2026";
 
 export async function GET() {
